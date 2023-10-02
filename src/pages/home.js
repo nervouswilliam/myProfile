a840674d0f03d980e9a43a740400b24ebdf3fa89
nervouswilliam <jeremiah.wsebastian@gmail.com>
2023-10-02 21:54:46 +0700

    issues still in the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,24 @@
+import styles from './css/Home.module.css';
+
 export default function home()
 {
-    return <h1>HOME</h1>
+    return(
+        <>
+            
+            <div className= {styles.container}>
+                <div className= {styles.pictures}>
+                    <img src = "../pictures/home.jpg"/>  
+                </div>
+                <div className= {styles.opening}>
+                    <h1>Hi, I'm William</h1>
+                    <p>
+                        I am a Computer Science Undergraduate focusing on Software Engineering.
+                        <br/>
+                        I am a full stack engineer, mostly coding in javascript in both front end and backend.
+                    </p>
+                </div>
+            </div>
+            
+        </>
+    )
 }
